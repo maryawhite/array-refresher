fe25d7aa9b7e8444f7cae25acc7f2feb76ada613
Mary White <mary.a.white28@gmail.com>
2021-09-17 23:25:39 -0400

    adding practice examples

--- js/array-refresher.js
+++ js/array-refresher.js
@@ -1,1 +1,20 @@
 "use strict"
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence);
+console.log(sequence.pop()); //returns 5 bc it returns what you popped off
+
+var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+
+// loop through the array and log the values
+for (var i = 0; i < shapes.length; i++) {
+    console.log('The shape at index ' + i + ' is: ' + shapes[i]);
+}
+
+var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+
+// loop through the array and log the values
+shapes.forEach(function(shape) {
+    console.log('Here is a lovely shape: ' + shape + '.');
+});
